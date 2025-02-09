903d5130af25ff82c5fb171a32bbc119d5c9a272
craftor <wnsjh9110@naver.com>
2025-02-10 00:11:34 +0900

    polynomial_prototype

---
+++ implement/polynomial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_POLYNOMIAL_SIZE 50
+
+typedef struct {
+    float coefficient;
+    int power;
+} polynomial;
+
+int main() {
+
+    polynomial* a;
+
+    return 0;
+}
