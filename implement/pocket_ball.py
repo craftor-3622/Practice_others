d593037c878c6b4cbf28ed701a3594e81714177f
craftor <wnsjh9110@naver.com>
2025-02-24 00:30:32 +0900

    update_250223

--- implement/pocket_ball.py
+++ implement/pocket_ball.py
@@ -12,6 +12,9 @@ class Vector:
     def __sub__(self, other):
         return Vector(self.x - other.x, self.y - other.y)
 
+    def __mul__(self, value: float):
+        return Vector(value * self.x, value * self.y)
+
     def multiply(self, coefficient: float):
         self.x *= coefficient
         self.y *= coefficient
@@ -19,16 +22,23 @@ class Vector:
     def magnitude(self) -> float:
         return math.sqrt(self.x**2 + self.y**2)
 
-    def dot_product(self, other):
-        return self.x * other.x + self.y * other.y
-
-    def normal_vector(self):
+    def unit_vector(self):
         x_n, y_n = self.x / self.magnitude(), self.y / self.magnitude()
         return Vector(x_n, y_n)
 
 
-def collision(a_ball: Vector, b_ball: Vector):
-    pass
+def dot_product(self, other) -> int:
+    return self.x * other.x + self.y * other.y
+
+
+def collision(a_pos: Vector, a_speed: Vector, b_pos: Vector):
+    normal_vec = Vector.unit_vector(b_pos - a_pos)
+    tangent_vec = Vector(-normal_vec.y, normal_vec.x)
+    
+    scalar_vn = dot_product(a_speed, normal_vec)
+    vector_vn = normal_vec * scalar_vn
+    scalar_vt = dot_product(a_speed, tangent_vec)
+    vector_vt = tangent_vec * scalar_vt
 
 
 def bound_wall():
@@ -46,7 +56,7 @@ Hole_angle = math.atan((hole.y - ball.y) / (hole.x - ball.x))
 
 Hit = Vector(ball.x - diameter*math.cos(Hole_angle), ball.y - diameter*math.sin(Hole_angle))
 Cushion = Vector(ball.x - diameter*math.cos(Hole_angle), ball.y + diameter*math.sin(Hole_angle))
-Hit_n = Hit.normal_vector()
+Hit_n = Hit.unit_vector()
 print(Hit.x, Hit.y)
 print(Hit_n.x, Hit_n.y)
 direction = Hit - my_ball
